c4b005c0c740179ea1dab218096fb0a7c1b5a5c1
Nidhiyadav07 <yadavnidhi9034@gmail.com>
2026-08-20 15:55:25 +0530

    Time: 0 ms (100%), Space: 52.9 MB (59.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,23 +4,32 @@ class Solution {
     }
 
     public int solve(int[] nums, int low, int high) {
+        
 
         if (low == high) {
             return nums[low];
         }
-
         int mid = low + (high - low) / 2;
 
-        if (mid % 2 == 1) {
-            mid--;
-        }
+        if(nums[mid]==nums[mid+1]){
+            if(mid%2==1){
+                return solve(nums, low, mid - 1);
+            }
+            else{
+                return solve(nums, mid + 2, high);
+            }
 
-      
-        if (nums[mid] == nums[mid + 1]) {
-            return solve(nums, mid + 2, high);
         }
+        else if (nums[mid] == nums[mid - 1]) {
 
-      
-        return solve(nums, low, mid);
+            if (mid % 2 == 1) {
+                return solve(nums, mid + 1, high);
+            } else {
+                return solve(nums, low, mid - 2);
+            }
+        }
+        else{
+            return nums[mid];
+        }
     }
 }
